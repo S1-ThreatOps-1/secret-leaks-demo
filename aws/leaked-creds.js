3d14d87cd85de1a70baa7d13adaf1e498a4c6a3d
calypso15 <ryan.ogrady@sentinelone.com>
2025-08-18 11:08:04 -0400

    Update AWS credentials in leaked-creds.js

--- aws/leaked-creds.js
+++ aws/leaked-creds.js
@@ -4,8 +4,9 @@ const aws = require('aws-sdk');
 
 // Configure AWS SDK with your credentials
 aws.config.update({
-  accessKeyId: 'AKIA2T2SJH6MXQGBHRPP',
-  secretAccessKey: 'icycDMWCD167itTgprHhOrRiCr9buJ1H8o4OHbmU',
-  region: 'us-east-2'});
+  accessKeyId: 'AKIA2T2SJH6M7ZGJORUW',
+  secretAccessKey: '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',
+  region: 'us-east-2'
+});
 
 const s3 = new aws.S3();
